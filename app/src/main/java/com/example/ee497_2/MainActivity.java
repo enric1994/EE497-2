3834600446585b593c2ee2a7ede46c0c3a0de3be
Enric Moreu <enric.moreu.filella@gmail.com>
2019-04-15 21:20:02 +0100

    clean main activity

--- app/src/main/java/com/example/ee497_2/MainActivity.java
+++ app/src/main/java/com/example/ee497_2/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.ee497_2;
 
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity {
 
 
     private GLSurfaceView gLView;
-    private SensorManager sensorManager;
-    private Sensor mAccelerometer;
 
 
     @Override
@@ -28,19 +21,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         gLView = new MyGLSurfaceView(this);
         setContentView(gLView);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        sensorManager.registerListener(this, mAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
-    }
-    @Override
-    public void onSensorChanged(SensorEvent event){
-
-        Log.d("mylog",String.valueOf(event.values[0]));
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
